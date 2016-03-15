87ee3d48247346a630e801bfaf5543c8ded4c789
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-03-15 16:21:45 +0000

    fixed spec

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
-
 feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
-    expect(page).to have_content 'Dave vs. Mittens'
+    fill_in :name1, with: 'Ash'
+    fill_in :name2, with: 'Garry'
+    click_button 'Fight'
+    expect(page).to have_content 'Ash vs. Garry'
   end
 end
