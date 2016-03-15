d3ad901b6abeb62d2f3ca36ac017e8229e33c339
Harsheet <harsheetpatel@hotmail.com>
2016-03-15 17:17:08 +0000

    Challenge 20 complete

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra/base'
 
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/check' do
     'Hello Battle!'
   end
@@ -12,13 +14,27 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player1 =params[:name1]
-    @player2 =params[:name2]
+    session[:name1] = params[:name1]
+    session[:name2] = params[:name2]
+    redirect '/play'
+  end
+
+  get '/play' do
+  @player1 =session[:name1]
+  @player2 =session[:name2]
     erb(:play)
   end
 
+
+  # get '/' do
+  #   "name =" session[:name1]
+  # end
+
+
+  # get '/:name2' do
+  #   session[:name2] = params[:name2]
+  # end
   # start the server if ruby file executed directly
   run! if app_file == $0
 
 end
-
--- spec/player_hit_points_spec.rb
+++ spec/player_hit_points_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-feature 'Enter names' do
-  scenario 'submitting names' do
-    visit('/')
+feature 'Hit Points'do
+  scenario 'See hit points of player 2' do
+    visit '/'
     fill_in :name1, with: 'Ash'
     fill_in :name2, with: 'Garry'
     click_button 'Fight'
-    expect(page).to have_content 'Ash vs. Garry'
+    expect(page).to have_content('20HP')
   end
 end
